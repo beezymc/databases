884958f82e5dc0c22803f891358c766df426ddcd
David Rajec <david.rajec@gmail.com>
2021-12-08 23:30:21 -0800

    Complete customizing tests.

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -16,7 +16,7 @@ describe('Persistent Node Chat Server', () => {
   beforeAll((done) => {
     dbConnection.connect();
 
-       const tablename = ''; // TODO: fill this out
+    const tablename = 'messages'; // TODO: fill this out
 
     /* Empty the db table before all tests so that multiple tests
      * (or repeated runs of the tests)  will not fail when they should be passing
@@ -65,8 +65,8 @@ describe('Persistent Node Chat Server', () => {
 
   it('Should output all messages from the DB', (done) => {
     // Let's insert a message into the db
-       const queryString = '';
-       const queryArgs = [];
+    const queryString = 'INSERT INTO users (username) VALUES ($); INSERT INTO rooms (roomname) VALUES ($); INSERT INTO messages (text, userId, roomId) VALUE ($, (SELECT id FROM users WHERE username = $), (SELECT id FROM rooms WHERE roomname = $));';
+    const queryArgs = ['joey', 'lobby', 'joey was here', 'joey', 'lobby'];
     /* TODO: The exact query string and query args to use here
      * depend on the schema you design, so I'll leave them up to you. */
     dbConnection.query(queryString, queryArgs, (err) => {
